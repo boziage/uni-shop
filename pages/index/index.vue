5cd81133ceae465704c65489952afc7724758504
boziage <423333770@qq.com>
2021-10-23 18:44:46 +0800

    build: 整合uview的网络请求,api集中管理,vuex与数据持久化

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,8 +9,19 @@
 		data() {
 			return {}
 		},
-		onLoad() {
-
+		async onLoad() {
+			// 测试api
+			// const data = {
+			// 	email: 'test@a.com',
+			// 	password: '123123'
+			// }
+			// const res = await this.$u.api.authLogin(data)
+			// console.log(res);
+			
+			// 测试vuex
+			console.log(this.vuex_version);
+			this.$u.vuex('vuex_name', 'bozi')
+			console.log(this.vuex_name);
 		},
 		methods: {
 
